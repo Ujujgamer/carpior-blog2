e381570ac6d6f9032698ec10e2365ef601598b57
Rolando Carpio <jujucarpio@sbcglobal.net>
2014-11-20 14:53:22 -0800

    Checked for error connections

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,5 +1,16 @@
 <?php
-    //selects the file we want to use
+    //selects the file with the information we want to use
     require_once("../model/database.php");
     //created a connection the server
     $connection = new mysqli($host, $user, $password);
+    //checks if there was a connection error; displays error message if there is
+    if($connection->connect_error) {
+        die("Error: " . $connection->connect_error);
+    }
+    //if no errors; display message and get host info
+    else {
+        echo "Success" . $connection->host_info;
+    }
+    
+    //closes the connection
+    $connection->close();
