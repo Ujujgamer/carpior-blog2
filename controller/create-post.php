dbb997676a48c5d11179d72000eb17d5f2296d29
Rolando Carpio <jujucarpio@sbcglobal.net>
2015-01-06 15:00:15 -0800

    Connected the controller to the form

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,8 @@
 <?php
     //created a vatiable for the title
-    $title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+    $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     //created a variable for the post
-    $post = filer_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+    $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     
     //displays the title
     echo "<p>Title: $title</p>";
