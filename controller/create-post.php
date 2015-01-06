c672f6c20c1a41c82e701dd3d62c7718cafe64ad
Rolando Carpio <jujucarpio@sbcglobal.net>
2015-01-06 15:05:05 -0800

    Queried our database to submit a blog post

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,8 @@
 <?php
+    require_once(__DIR__ . "/../model/database.php");
+    
+    $connection = new mysqli($host, $username, $password, $database);
+    
     //created a vatiable for the title
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     //created a variable for the post
@@ -8,3 +12,5 @@
     echo "<p>Title: $title</p>";
     //displays the post
     echo "<p>Post: $post</p>";
+    
+    $connection->close();
